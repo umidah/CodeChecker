bc04fbd87d8bd0dac625dc0cbe81e8b941895716
umidah <46555380+umidah@users.noreply.github.com>
2020-02-13 12:40:13 +0800

    v3 (tentative)

---
+++ QtCodeCheckerV3/mainmenu.cpp
@@ -0,0 +1,15 @@
+#include "mainmenu.h"
+#include "ui_mainmenu.h"
+
+MainMenu::MainMenu(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainMenu)
+{
+    ui->setupUi(this);
+}
+
+MainMenu::~MainMenu()
+{
+    delete ui;
+}
+
---
+++ QtCodeCheckerV3/mainmenu.h
@@ -0,0 +1,21 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainMenu; }
+QT_END_NAMESPACE
+
+class MainMenu : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainMenu(QWidget *parent = nullptr);
+    ~MainMenu();
+
+private:
+    Ui::MainMenu *ui;
+};
+#endif // MAINMENU_H
